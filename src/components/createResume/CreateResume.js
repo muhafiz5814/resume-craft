099bfbc49ea734d28b5c55a7e729ff56a3b7d52c
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-07-22 07:06:58 +0530

    Move all the form components in saperate folder

--- src/components/createResume/CreateResume.js
+++ src/components/createResume/CreateResume.js
@@ -1,29 +1,27 @@
-import Layout from "../Layout"
-import AboutMeForm from "./AboutMeForm"
-import BasicInfoForm from "./BasicInfoForm"
-import EducationForm from "./EducationForm"
-import ExperienceForm from "./ExperienceForm"
-import Form from "./ProjectsForm"
-import SkillsForm from "./SkillsForm"
-import { useAddResumeMutation } from "../../store/apiSlice"
-import { useDispatch, useSelector } from "react-redux"
-import { clearTempResume } from "../../store/tempResumeSlice"
-import { useNavigate } from "react-router-dom"
-
+import Layout from "../Layout";
+import AboutMeForm from "../formComponents/AboutMeForm";
+import BasicInfoForm from "../formComponents/BasicInfoForm";
+import EducationForm from "../formComponents/EducationForm";
+import ExperienceForm from "../formComponents/ExperienceForm";
+import Form from "../formComponents/ProjectsForm";
+import SkillsForm from "../formComponents/SkillsForm";
+import { useAddResumeMutation } from "../../store/apiSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { clearTempResume } from "../../store/tempResumeSlice";
+import { useNavigate } from "react-router-dom";
 
 const CreateResume = () => {
-
-  const navigate = useNavigate()
-  const resume = useSelector(state => state.tempResume)
-  const [addResume, {isLoading}] = useAddResumeMutation()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const resume = useSelector((state) => state.tempResume);
+  const [addResume, { isLoading }] = useAddResumeMutation();
+  const dispatch = useDispatch();
 
   const handleSubmit = (evt) => {
-    evt.preventDefault()
-    addResume(resume)
-    dispatch(clearTempResume())
-    navigate("/")
-  }
+    evt.preventDefault();
+    addResume(resume);
+    dispatch(clearTempResume());
+    navigate("/");
+  };
 
   return (
     <Layout>
@@ -37,7 +35,7 @@ const CreateResume = () => {
         <button type="submit">Submit</button>
       </form>
     </Layout>
-  )
-}
+  );
+};
 
-export default CreateResume
+export default CreateResume;
--- src/components/updateResume/UpdateResume.js
+++ src/components/updateResume/UpdateResume.js
@@ -1,32 +1,34 @@
-import { useEffect } from "react"
-import Layout from "../Layout"
-import AboutMeForm from "../createResume/AboutMeForm"
-import BasicInfoForm from "../createResume/BasicInfoForm"
-import EducationForm from "../createResume/EducationForm"
-import ExperienceForm from "../createResume/ExperienceForm"
-import ProjectsForm from "../createResume/ProjectsForm"
-import SkillsForm from "../createResume/SkillsForm"
-import { useParams, useNavigate } from "react-router-dom"
-import { useGetResumeQuery, useUpdateResumeMutation } from "../../store/apiSlice"
-import { useDispatch, useSelector } from "react-redux"
-import { setTempResume, clearTempResume } from "../../store/tempResumeSlice"
+import { useEffect } from "react";
+import Layout from "../Layout";
+import AboutMeForm from "../formComponents/AboutMeForm";
+import BasicInfoForm from "../formComponents/BasicInfoForm";
+import EducationForm from "../formComponents/EducationForm";
+import ExperienceForm from "../formComponents/ExperienceForm";
+import ProjectsForm from "../formComponents/ProjectsForm";
+import SkillsForm from "../formComponents/SkillsForm";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  useGetResumeQuery,
+  useUpdateResumeMutation,
+} from "../../store/apiSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setTempResume, clearTempResume } from "../../store/tempResumeSlice";
 
 const UpdateResume = () => {
-
-  const {id} = useParams()
-  const navigate = useNavigate()
-  console.log("id",id)
-  const dispatch = useDispatch()
-  const resumeData = useSelector(state => state.tempResume)
-  console.log("resumeData at start ", resumeData)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  console.log("id", id);
+  const dispatch = useDispatch();
+  const resumeData = useSelector((state) => state.tempResume);
+  console.log("resumeData at start ", resumeData);
   let loaded = false;
 
-  const {data, isLoading, error} = useGetResumeQuery(id)
-  const [updateResume, {isloading}] = useUpdateResumeMutation()
+  const { data, isLoading, error } = useGetResumeQuery(id);
+  const [updateResume, { isloading }] = useUpdateResumeMutation();
 
-  console.log("Before useeffect 1 data", data)
+  console.log("Before useeffect 1 data", data);
   useEffect(() => {
-    console.log("inside use effect 1")
+    console.log("inside use effect 1");
     if (data) {
       const resume = {
         basicInfo: data.basicInfo,
@@ -35,16 +37,15 @@ const UpdateResume = () => {
         experience: data.experience,
         projects: data.projects,
         skills: data.skills,
-      }
-      console.log("resume: ", resume)
+      };
+      console.log("resume: ", resume);
       dispatch(setTempResume(resume));
-      console.log("After the dispatch")
+      console.log("After the dispatch");
     }
   }, [data, dispatch]);
-  console.log("After useeffect 1 data", data)
+  console.log("After useeffect 1 data", data);
 
-
-  console.log("Loaded before use effect2: ", loaded)
+  console.log("Loaded before use effect2: ", loaded);
 
   // useEffect(() => {
   //   console.log("inside use effect 2")
@@ -55,21 +56,19 @@ const UpdateResume = () => {
   //   }
   // }, [resumeData, loaded])
 
-  console.log("Loaded After use effect2: ", loaded)
+  console.log("Loaded After use effect2: ", loaded);
   const handleSubmit = (evt, id) => {
-    evt.preventDefault()
-    console.log(resumeData)
-    updateResume({id, updatedResume: {id, ...resumeData}})
-    dispatch(clearTempResume())
-    navigate("/")
-  }
-
-  
+    evt.preventDefault();
+    console.log(resumeData);
+    updateResume({ id, updatedResume: { id, ...resumeData } });
+    dispatch(clearTempResume());
+    navigate("/");
+  };
 
   return (
     <Layout>
-    {resumeData.aboutMe 
-      ? <form onSubmit={(evt) => handleSubmit(evt, id)}>
+      {resumeData.aboutMe ? (
+        <form onSubmit={(evt) => handleSubmit(evt, id)}>
           <BasicInfoForm />
           <AboutMeForm />
           <EducationForm />
@@ -78,12 +77,11 @@ const UpdateResume = () => {
           <SkillsForm />
           <button type="submit">Submit</button>
         </form>
-      : <h2>Loading...</h2>
-
-    }
-      
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </Layout>
-  )
-}
+  );
+};
 
-export default UpdateResume
+export default UpdateResume;
